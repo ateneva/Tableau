b1618c057849ab12dd26d79453b5df609ad72939
ateneva <tenevaa21@gmail.com>
2021-02-11 23:10:12 +0100

    eliminate double sign-in

--- APIs/06. generate wbk pdf.py
+++ APIs/06. generate wbk pdf.py
@@ -9,7 +9,7 @@ def download_as_pdf(tableau_server, tableau_user, user_password, site_name, down
 
     now = dt.datetime.today().strftime('%Y-%m-%d %H-%M-%S')
     today = dt.datetime.today().strftime('%Y-%m-%d')
-    print(today, now
+    print(today, now)
 
     # compile list of workbooks to download as pdf
     generate_pdf = []
@@ -26,19 +26,18 @@ def download_as_pdf(tableau_server, tableau_user, user_password, site_name, down
             req_option = TSC.RequestOptions()
             req_option.filter.add(TSC.Filter(TSC.RequestOptions.Field.Name, TSC.RequestOptions.Operator.Equals, item))
 
-            with server.auth.sign_in(tableau_auth):
-                published, pagination_item = server.workbooks.get(req_option)
-                print("\nThere are {} workbooks on site: ".format(pagination_item.total_available))
+            published, pagination_item = server.workbooks.get(req_option)
+            print("\nThere are {} workbooks on site: ".format(pagination_item.total_available))
 
-                for wbk in published:
-                    server.workbooks.populate_connections(wbk)
+            for wbk in published:
+                server.workbooks.populate_connections(wbk)
 
-                    # get the used datasources in the wbk
-                    connection_info = [connection.datasource_name for connection in wbk.connections]
-                    print(connection_info)
+                # get the used datasources in the wbk
+                connection_info = [connection.datasource_name for connection in wbk.connections]
+                print(connection_info)
 
-                    if len(connection_info) > 0:
-                        print(
+                if len(connection_info) > 0:
+                    print(
                             wbk.id,
                             wbk.name, '_',
                             wbk.connections[0].connection_type,'_',
@@ -47,29 +46,29 @@ def download_as_pdf(tableau_server, tableau_user, user_password, site_name, down
                             wbk.connections[0].server_address
                               )
 
-                    # specify PDF landsscape
-                    pdf_req_option = TSC.PDFRequestOptions(page_type=TSC.PDFRequestOptions.PageType.A4,
+                # specify PDF landsscape
+                pdf_req_option = TSC.PDFRequestOptions(page_type=TSC.PDFRequestOptions.PageType.A4,
                                                            orientation=TSC.PDFRequestOptions.Orientation.Landscape)
 
-                    # download wbk as separate page PDFs
-                    pdf_files = []
-                    server.workbooks.populate_views(wbk)
+                # download wbk as separate page PDFs
+                pdf_files = []
+                server.workbooks.populate_views(wbk)
 
-                    for v in wbk.views:
-                        print(v.id, ' | ', v.name)
-                        pdf_files.append(v.name)
-                        server.views.populate_pdf(v, pdf_req_option)
-                        saves = download_path + v.name + '.pdf'
+                for v in wbk.views:
+                    print(v.id, ' | ', v.name)
+                    pdf_files.append(v.name)
+                    server.views.populate_pdf(v, pdf_req_option)
+                    saves = download_path + v.name + '.pdf'
 
-                        with open(saves, 'wb') as f:
-                            f.write(v.pdf)
+                    with open(saves, 'wb') as f:
+                        f.write(v.pdf)
 
-                    print(f'{len(pdf_files)} files downloaded, {pdf_files}')
+                print(f'{len(pdf_files)} files downloaded, {pdf_files}')
 
-                    # merge all PDFs
-                    mergedObject = PdfFileMerger()
+                # merge all PDFs
+                mergedObject = PdfFileMerger()
 
-                    for f in pdf_files:
-                        mergedObject.append(PdfFileReader(download_path + f + '.pdf', 'rb'))
+                for f in pdf_files:
+                    mergedObject.append(PdfFileReader(download_path + f + '.pdf', 'rb'))
 
-                    mergedObject.write(download_path + str(today) + ' ' + wbk.name + '.pdf')
+                mergedObject.write(download_path + str(today) + ' ' + wbk.name + '.pdf')
