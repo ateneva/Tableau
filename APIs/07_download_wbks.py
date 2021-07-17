391bb9dd9caefac562c9d5561a29266f0bdb179e
ateneva <tenevaa21@gmail.com>
2021-07-17 18:23:32 +0200

    remove code redundancies in authentication

--- APIs/07_download_wbks.py
+++ APIs/07_download_wbks.py
@@ -4,14 +4,21 @@ import datetime as dt
 now = dt.datetime.today().strftime('%Y-%m-%d %H-%M-%S')
 print(now)
 
-def download_wbks_from_owner(tableau_server, tableau_user, user_password, site_name, download_path, owner):
-    # if you're connecting to the default site, pass empty string in site_name
-    # download path - e.g. 'C:/Users/angelinat/Desktop/TESTS/'
+tableau_server = 'tableau server link'
+tableau_user = 'tableau admin username'
+user_password = 'tableau admin credentials'
+site_name = 'tableau site name'
+download_path = 'C:/Users/angelinat/Desktop/TESTS/'
 
-    # authenticate with Tableau Server
-    tableau_auth = TSC.TableauAuth(tableau_user, user_password, site_id=site_name)
-    server = TSC.Server(tableau_server, use_server_version=True)
+# if you're connecting to the default site, pass empty string in site_name
+# download path - e.g. 'C:/Users/angelinat/Desktop/TESTS/'
 
+# authenticate with Tableau Server
+tableau_auth = TSC.TableauAuth(tableau_user, user_password, site_id=site_name)
+server = TSC.Server(tableau_server, use_server_version=True)
+
+
+def download_wbks_from_owner(owner):
     with server.auth.sign_in(tableau_auth):
         all_users, pagination_item = server.users.get()     # get all users on site
         user_names = [user.name for user in all_users]
@@ -32,12 +39,9 @@ def download_wbks_from_owner(tableau_server, tableau_user, user_password, site_n
                     print(f'Downloaded {wbk_name}')
 
 
-def download_wbks_from_project(tableau_server, tableau_user, user_password, site_name, download_path, project):
-    # authenticate with Tableau Server
-    tableau_auth = TSC.TableauAuth(tableau_user, user_password, site_id=site_name)
-    server = TSC.Server(tableau_server, use_server_version=True)
-
+def download_wbks_from_project(project):
     with server.auth.sign_in(tableau_auth):
+
         # page through all the workbooks on the Server and keep the relevant ones
         wbks_to_review = {wbk.name: wbk.id for wbk in TSC.Pager(server.workbooks) if wbk.project_name == project}
         print(f"{[k for k in wbks_to_review.keys()]}")
@@ -48,11 +52,7 @@ def download_wbks_from_project(tableau_server, tableau_user, user_password, site
             print(f'Downloaded {wbk_name}')
 
 
-def download_wbks_connected_to(tableau_server, tableau_user, user_password, site_name, download_path, datasource):
-    # authenticate with Tableau Server
-    tableau_auth = TSC.TableauAuth(tableau_user, user_password, site_id=site_name)
-    server = TSC.Server(tableau_server, use_server_version=True)
-
+def download_wbks_connected_to(datasource):
     with server.auth.sign_in(tableau_auth):
         all_workbooks = {wbk.name: wbk.id for wbk in TSC.Pager(server.workbooks)}
 
